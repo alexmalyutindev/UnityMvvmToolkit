ee405ae47c297fd31b57f00f6374f7fffe2542a6
Alex Malyutin <alex.malyutin.dev@gmail.com>
2023-05-02 17:49:23 +0400

    Test source generator.

---
+++ src/UnityMvvmToolkit.UnityPackage/Assets/Plugins/UnityMvvmToolkit/Runtime/Core/GenObjectProvider.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Reflection;
+using UnityMvvmToolkit.Core.Enums;
+using UnityMvvmToolkit.Core.Interfaces;
+
+namespace UnityMvvmToolkit.Core
+{
+    public class GenObjectProvider : IObjectProvider, IDisposable
+    {
+        public IObjectProvider WarmupAssemblyViewModels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObjectProvider WarmupAssemblyViewModels(Assembly assembly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObjectProvider WarmupViewModel<TBindingContext>() where TBindingContext : IBindingContext
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObjectProvider WarmupViewModel(Type bindingContextType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObjectProvider WarmupValueConverter<T>(int capacity, WarmupType warmupType = WarmupType.OnlyByType) where T : IValueConverter
+        {
+            throw new NotImplementedException();
+        }
+
+        public IProperty<TValueType> RentProperty<TValueType>(IBindingContext context, PropertyBindingData bindingData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReturnProperty<TValueType>(IProperty<TValueType> property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyProperty<TValueType> RentReadOnlyProperty<TValueType>(IBindingContext context, PropertyBindingData bindingData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReturnReadOnlyProperty<TValueType>(IReadOnlyProperty<TValueType> property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TCommand GetCommand<TCommand>(IBindingContext context, string propertyName) where TCommand : IBaseCommand
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBaseCommand RentCommandWrapper(IBindingContext context, CommandBindingData bindingData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReturnCommandWrapper(IBaseCommand command, CommandBindingData bindingData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue GetCollectionItemTemplate<TKey, TValue>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/UnityMvvmToolkit/UnityMvvmToolkit.Gen/BindingsGenerator.cs
@@ -0,0 +1,115 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace UnityMvvmToolkit.Gen;
+
+[Generator]
+public class BindingsGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        context.RegisterForSyntaxNotifications(() => new BindableAttributeReceiver());
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        if (context.SyntaxReceiver is BindableAttributeReceiver receiver)
+        {
+            foreach (var classInfo in receiver.ClassesWithAttributes)
+            {
+                Console.WriteLine(">> " + classInfo.Name);
+                var source = new StringBuilder();
+
+                if (!String.IsNullOrEmpty(classInfo.Namespace))
+                {
+                    source.Append("namespace ").Append(classInfo.Namespace).AppendLine("{");
+                }
+
+                source.AppendLine(@"using UnityMvvmToolkit.Core;
+using UnityMvvmToolkit.Core.Enums;
+using UnityMvvmToolkit.Core.Interfaces;"
+                );
+
+                source.AppendLine("public partial class " + classInfo.Name + " : IObjectProvider");
+                source.AppendLine("{");
+
+
+                // source.AppendLine(@"public TCommand GetCommand<TCommand>(IBindingContext context, string propertyName) where TCommand : IBaseCommand
+                // {
+                //     throw new NotImplementedException();
+                // }");
+                
+                source.AppendLine("}");
+                
+                if (!String.IsNullOrEmpty(classInfo.Namespace))
+                {
+                    source.AppendLine("}");
+                }
+                
+                context.AddSource(
+                    classInfo.Name + ".gen.cs",
+                    source.ToString()
+                );
+            }
+        }
+    }
+}
+
+public class BindableAttributeReceiver : ISyntaxReceiver
+{
+    public class ClassInfo
+    {
+        public ClassDeclarationSyntax ClassNode;
+        public string Name;
+        public string Namespace;
+        public string TargetType;
+    }
+
+    public List<ClassInfo> ClassesWithAttributes = new();
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is not ClassDeclarationSyntax classNode) return;
+
+        bool hasAttribute = false;
+        foreach (var attribute in classNode.AttributeLists)
+        {
+            foreach (var attr in attribute.Attributes)
+            {
+                // TODO: Create and inject 'GenerateMVVMBinding' via source generator
+                if (attr.Name is IdentifierNameSyntax { Identifier.Text: "GenerateMVVMBinding" })
+                {
+                    Console.WriteLine("[CLASS] " + classNode);
+                    hasAttribute = true;
+                    break;
+                }
+            }
+
+            if (hasAttribute)
+            {
+                break;
+            }
+        }
+
+        if (hasAttribute)
+        {
+            var namespaceName = "";
+            if (classNode.Parent is NamespaceDeclarationSyntax namespaceNode)
+            {
+                namespaceName = namespaceNode.Name.ToFullString();
+            }
+
+            ClassesWithAttributes.Add(
+                new ClassInfo
+                {
+                    ClassNode = classNode,
+                    Name = classNode.Identifier.Text,
+                    Namespace = namespaceName
+                }
+            );
+            Console.WriteLine($"[COLLECT] {namespaceName}::{classNode.Identifier.Text}");
+        }
+    }
+}
